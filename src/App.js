9ff4d4c1a158a0016df21f9e3981ea3600716ec7
Francesco Menghi <53121061+menghif@users.noreply.github.com>
2023-07-16 11:30:57 -0400

    fixed active selection in NavBar

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ import NotFound from "./NotFound";
 function App() {
   const [searchString, setSearchString] = useState("");
   const history = useHistory();
+  const [activeKey, setActiveKey] = useState("/");
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -31,19 +32,29 @@ function App() {
     setSearchString("");
   }
 
+  const handleBrandClick = () => {
+    setActiveKey("");
+  };
+
   return (
     <>
       <Navbar bg="secondary" variant="dark" expand="lg">
         <LinkContainer to="/">
-          <Navbar.Brand>New York Restaurants</Navbar.Brand>
+          <Navbar.Brand onClick={handleBrandClick}>
+            New York Restaurants
+          </Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <LinkContainer to="/restaurants">
+          <Nav
+            className="mr-auto"
+            activeKey={activeKey}
+            onSelect={(selectedKey) => setActiveKey(selectedKey)}
+          >
+            <LinkContainer to="/restaurants" eventKey="/restaurants">
               <Nav.Link>Full List</Nav.Link>
             </LinkContainer>
-            <LinkContainer to="/about">
+            <LinkContainer to="/about" eventKey="/about">
               <Nav.Link>About</Nav.Link>
             </LinkContainer>
           </Nav>
