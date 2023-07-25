99fcfc41575ced2e436768571add934e6936ab6c
Francesco Menghi <53121061+menghif@users.noreply.github.com>
2023-07-25 13:10:13 -0400

    add restaurants.js to test getServerSideProps

---
+++ pages/restaurants.js
@@ -0,0 +1,36 @@
+import clientPromise from "../lib/mongodb";
+
+export default function Restaurants({ restaurants }) {
+  return (
+    <div>
+      <h1>List of Restaurants</h1>
+      <ul>
+        {restaurants.map((restaurant) => (
+          <li key={restaurant._id}>
+            <h2>{restaurant.name}</h2>
+            <p>{restaurant.borough}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  try {
+    const client = await clientPromise;
+    const db = client.db("sample_restaurants");
+
+    const restaurants = await db
+      .collection("restaurants")
+      .find({})
+      .limit(20)
+      .toArray();
+
+    return {
+      props: { restaurants: JSON.parse(JSON.stringify(restaurants)) },
+    };
+  } catch (e) {
+    console.error(e);
+  }
+}
