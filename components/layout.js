4fe7317690eb9e12c669c65cb433927d899150a9
Francesco Menghi <53121061+menghif@users.noreply.github.com>
2023-07-31 20:09:10 -0400

    Fix navbar

--- components/layout.js
+++ components/layout.js
@@ -1,14 +1,14 @@
 import {
   Button,
-  Col,
   Container,
   Form,
   FormControl,
   Nav,
   Navbar,
+  Row,
+  Col,
 } from "react-bootstrap";
 import { useRouter } from "next/router";
-
 import { useState } from "react";
 
 export default function Layout({ children }) {
@@ -28,41 +28,54 @@ export default function Layout({ children }) {
   }
 
   return (
-    <div>
-      <Navbar bg="secondary" variant="dark" expand="lg">
-        <Container>
+    <>
+      <Navbar bg="secondary" data-bs-theme="dark" expand="md">
+        <Container fluid xs="auto">
           <Navbar.Brand href="/">New York Restaurants</Navbar.Brand>
-        </Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav
-            className="mr-auto"
-            activeKey={activeKey}
-            onSelect={(selectedKey) => setActiveKey(selectedKey)}
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-between"
           >
-            <Nav.Link href="/restaurants" active={isActive("/restaurants")}>
-              <h2>Full List</h2>
-            </Nav.Link>
-            <Nav.Link href="/about" active={isActive("/about")}>
-              <h2>About</h2>
-            </Nav.Link>
-          </Nav>
-          <Form onSubmit={handleSubmit} className="search-box">
-            <FormControl
-              type="text"
-              placeholder="Borough"
-              className="mr-sm-2"
-              value={searchString}
-              onChange={(e) => setSearchString(e.target.value)}
-            />
-            <Button type="submit" variant="info" className="search-button">
-              Search
-            </Button>
-          </Form>
-        </Navbar.Collapse>
+            <Nav
+              className="me-auto"
+              activeKey={activeKey}
+              onSelect={(selectedKey) => setActiveKey(selectedKey)}
+            >
+              <Nav.Link href="/restaurants" active={isActive("/restaurants")}>
+                Full List
+              </Nav.Link>
+              <Nav.Link href="/about" active={isActive("/about")}>
+                About
+              </Nav.Link>
+            </Nav>
+            <Form onSubmit={handleSubmit} className="search-box">
+              <Row xs="auto">
+                <Col xs="auto">
+                  <FormControl
+                    type="text"
+                    data-bs-theme="light"
+                    placeholder="Borough"
+                    value={searchString}
+                    onChange={(e) => setSearchString(e.target.value)}
+                  />
+                </Col>
+                <Col xs="auto" className="search-button-col">
+                  <Button
+                    type="submit"
+                    variant="info"
+                    className="search-button"
+                  >
+                    Search
+                  </Button>
+                </Col>
+              </Row>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
 
       {children}
-    </div>
+    </>
   );
 }
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -7,7 +7,7 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Layout>
-        <Component {...pageProps} />;
+        <Component {...pageProps} />
       </Layout>
     </>
   );
